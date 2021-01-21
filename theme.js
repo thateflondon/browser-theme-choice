ef1ca184f7b879455b3f8873d26ef207cc36f190
Jerry Frederic <jerry.fpl@gmail.com>
2021-01-21 18:05:11 +0100

    app done

--- theme.js
+++ theme.js
@@ -1,7 +1,21 @@
 let boutton = document.querySelector('#mode');
 let span = document.querySelector('span');
 
+//3.Sauvegarde du dernier réglage de l'user
+if(localStorage.getItem('theme')) {
+    if(localStorage.getItem('theme') == 'sombre') {
+        modeSombre();
+    }
+}
 
+//Autre manière de mettre en place la fonctionnalité de sauvegarde
+// if (localStorage.getItem('theme') == 'sombre') {
+//     modeSombre();
+// }else{
+//     localStorage.getItem('theme') == 'clair';
+// }
+
+//1.Ecoute du boutton
 boutton.addEventListener('click', () => {
     if(document.body.classList.contains('dark')) {
         //Mode clair
@@ -14,8 +28,14 @@ boutton.addEventListener('click', () => {
     }
 });
 
+//2.Création de la fonction modeSombre
 function modeSombre() {
     document.body.className = 'dark';
     span.textContent = 'Thème Clair';
     localStorage.setItem('theme', 'sombre');
+    // if (localStorage.getItem('theme') == 'sombre') {
+    //     modeSombre();
+    // }else{
+    //     localStorage.setItem('theme') == 'clair';
+    // }
 }
